b3f1235c4349149afc7ca89fd44bddc74f2d8f68
machs <460286423@qq.com>
2020-02-02 17:39:35 +0800

    add proj

---
+++ proj.ios_mac/Pods/Target Support Files/Pods-iTC_test-desktop/Pods-iTC_test-desktop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTC_test_desktop : NSObject
+@end
+@implementation PodsDummy_Pods_iTC_test_desktop
+@end
---
+++ proj.ios_mac/Pods/Target Support Files/Pods-iTC_test-mobile/Pods-iTC_test-mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTC_test_mobile : NSObject
+@end
+@implementation PodsDummy_Pods_iTC_test_mobile
+@end
